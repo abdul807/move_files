80984af4454ab7447a07cfacee4c83da0eaa77b3
Usman Abdul Matin <amatin832@gmail.com>
2023-03-10 08:15:22 -0800

    Move a specified file from a folder from another

---
+++ main.py
@@ -0,0 +1,25 @@
+import shutil
+import sys
+import os
+
+
+download_folder = os.path.expanduser("~/downloads")
+zipped_files = os.path.expanduser("~/desktop/zipped")
+
+
+if not os.path.exists(zipped_files):
+    os.makedirs(zipped_files)
+
+for file_name in os.listdir(download_folder):
+    if file_name.endswith(".zip"):
+        file_path = os.path.join(download_folder, file_name)
+        # print(file_path)
+        shutil.move(file_path,zipped_files)
+
+print('finished')
+
+
+# print(os.listdir(zipped_files))
+
+# print(download_folder)
+# print(zipped_files)
